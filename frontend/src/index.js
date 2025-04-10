c2664733175536725846e32e38bfc856b15d1eb8
Jeffrey Johnston <a7a5096@googlemail.com>
2025-04-10 06:46:40 -0600

    Add frontend source files (index.js, App.js, etc.)

---
+++ frontend/src/index.js
@@ -0,0 +1,19 @@
+// frontend/src/index.js
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css'; // Optional: if you have a global CSS file
+import App from './App'; // Import your main App component
+import reportWebVitals from './reportWebVitals'; // Optional: for performance monitoring
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
